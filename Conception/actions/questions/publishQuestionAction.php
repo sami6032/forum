52c293df7fa53998a4795b3e0e360a623af07fb6
sabine <konkobosabine29@gmail.com>
2023-06-27 13:42:59 +0000

    ajout de fichier

---
+++ Conception/actions/questions/publishQuestionAction.php
@@ -0,0 +1,34 @@
+<?php 
+require('actions/database.php'); 
+
+if(isset($_POST['validate'])){
+
+    if(!empty($_POST['title']) AND !empty($_POST['description']) AND !empty($_POST['content']))
+      $question_title = htmlspecialchars($_POST['title']);
+      $question_description = nl2br(htmlspecialchars($_POST['description']));
+      $question_contenu = nl2br(htmlspecialchars($_POST['content']));
+      $question_date = date('d/m/y');
+      $question_id_auteur = $_SESSION['id'];
+      $question_pseudo_auteur = $_SESSION['pseudo'];
+      
+      $insertQuestionOnwebsite = $bdb->prepare('INSERT INTO questions(titre, description, contenu, id_auteur, pseudo_auteur, date_publication)VALUES(?, ?, ?, ?, ?, ?)');
+      $insertQuestionOnwebsite->execute(
+        array(
+            $question_title,
+            $question_description,
+            $question_contenu,  
+            $question_id_auteur, 
+            $question_pseudo_auteur, 
+            $question_date
+        )
+     );
+
+     $successMsg = "votre question a bien été publié sur le site";
+     
+  }else{
+         $errorMsg = "veuillez compléter tous les champs...";
+}
+
+
+
+?>
---
+++ Conception/publish_question.php
@@ -0,0 +1,36 @@
+<?php
+  // require('actions/securityAction.php');
+ require('actions/questions/publishQuestionAction.php');
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="assets/publish.css">
+</head>
+<body>
+
+<form class="container" method="POST">
+    <?php 
+    if(isset($_errorMsg)){
+        echo '<p>'.$_errorMsg.'<p>';
+      }elseif(isset($successMsg)){
+        echo '<p>'.$_successMsg.'<p>';
+      }
+
+
+          ?>
+        <label>Titre de la question</label>
+        <input type="text" name ="title">
+        <label>Description de la question</label>
+        <input type="text" name ="description">
+        <label>Contenu de la question</label>
+        <input type="text" name ="content">
+
+        <button type="submit" href="" name="validate"> Publier la question</button>
+    </form>
+</body>
+</html>
