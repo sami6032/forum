d29342f6b1e39f31535a40c386a932a96c08a2cf
MAIGA_Samira <maigasamira6@gmail.com>
2023-06-27 10:01:59 +0000

    second commit

--- Conception/signup.php
+++ Conception/signup.php
@@ -1,24 +1,29 @@
+<?php require ('actions/signupAction.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forum</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="assets/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-<div class="row " style= "" >
+<div class="row mt-5 " style= "" >
             <div class="col-md-12 corppage ">
                 <div class="card corpform">
-                    <div class="card-header9">
+                    <!-- <div class="card-header9">
                         <h4>Ajouter 
                             <a href="../index.php" class="btn  float-end"><strong>Retour</strong></a>
                         </h4>
-                    </div>
-                    <div class="card-body" style= "background-color: #D90000; " >
-                        <form action="../php/code.php" method="POST" onsubmit="return confirmerModif();">
-
+                    </div> -->
+                    <div class="card-body" style= "background-color: #0100C3; " >
+                        <form action="" method="POST">
+                        <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>';}?>
+                        <div class="mb-3 ff">
+                                <!-- <label> pseudo</label> -->
+                                <input type="text" name="pseudo" required placeholder="Entrer votre pseudo" class="form-control w-50px text-center">
+                            </div>
                             <div class="mb-3 ff">
                                 <!-- <label> Nom</label> -->
                                 <input type="text" name="nom" required placeholder="Entrer votre nom" class="form-control w-50px text-center">
@@ -29,19 +34,11 @@
                             </div>
                             <div class="mb-3">
                                 <!-- <label>Date De Naissance</label> -->
-                                <input type="date" name="date_de_naissance" placeholder="Entrer votre date de naissance" class="form-control text-center">
-                            </div>
-                            <div class="mb-3">
-                                <!-- <label>Ville d'Origine</label> -->
-                                <input type="text" name="ville_origine" placeholder="Entrer votre ville d'origine" class="form-control text-center">
-                            </div>
-                            <div class="mb-3">
-                                <!-- <label>Formation de Base </label> -->
-                                <input type="text" name="formation_de_base" placeholder="Entrer votre formation de base" class="form-control text-center">
+                                <input type="text" name="mdp" placeholder="Entrer votre mot de passe" class="form-control text-center">
                             </div>
                            
                             <div class="mb-3 text-center ">
-                                <button type="submit" class="btn btn-light text-center" >ENVOYEZ</button>
+                                <button type="submit" class="btn btn-light text-center" name="validate" >S'inscrire</button>
                             </div>
                             </div>
                         </form>
