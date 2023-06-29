b3c9eda22fd647010480e04d38464dd3cb137c72
MAIGA_Samira <maigasamira6@gmail.com>
2023-06-29 15:31:55 +0000

    commit suivant

--- Conception/index.php
+++ Conception/index.php
@@ -4,37 +4,102 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenus sur ce forum</title>
+    <title>G1_forum</title>
+    <link rel="shortcut icon" href="images/world mission logo - Fait avec PosterMyWall.jpg" type="image/x-icon">
     <link rel="stylesheet" href="assets/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary" >
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img width="100px" src="images/world mission logo - Fait avec PosterMyWall.jpg" alt=""></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"></a>
-        </li>
-        
-      </ul>
-      <div class="" style= "display-flex; justify-content:space-between;">
-        <p><strong>Connexion</strong></p>
-        <p><strong>Inscription</strong></p>
-      
-    
-      </div> 
-    </div>
+<!-- <nav class="navbar bg-body-ligth">
+  <div class="container">
+    <a href="" class="navbar-brand"><img width="100px" src="images/world mission logo - Fait avec PosterMyWall.jpg" alt=""></a>
+    <a href="" class="navbar-brand"><strong>Connexion</strong></a>
+    <a href="" class="navbar-brand"><strong>Inscription</strong></a>
+    <a href=""></a>
   </div>
 </nav>
+<div>
+  <img width="100%" height="520px" src="images/pexels-gabriel-peter-696644.jpg" alt="">
+  <p class="bienvenus"> <strong>BIENVENUS SUR CE FORUM D'ECHANGE</strong></p>
+</div> -->
 
+<header class="mia">
+    <nav class="navbar navbar-expand-lg " style="background-color: white;";>
+      <div class="container">
+        <a class="navbar-brand" href="#"><img width="100px" src="images/world mission logo - Fait avec PosterMyWall.jpg" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+          aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+           
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                <div class="dropdown">
+                  <button class="btn btn-secondary " type="button" id="dropdownMenu2" data-bs-toggle="dropdown"
+                    aria-expanded="false" style="background-color: white;color: #0200C3";>
+                    <strong>Questions</strong>
+                  </button>
+
+                </div>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="publish_question.php">
+                <div>
+                  <button class="btn btn-secondary " type="button"style="background-color: white;color: #0200C3"; >
+                    <strong>Publier une question</strong>
+                  </button>
+                </div>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                <div>
+                  <button class="btn btn-secondary " type="button" style="background-color: white;color: #0200C3";>
+                    <strong>Mes questions</strong>
+                  </button>
+
+                </div>
+              </a>
+            </li>
+          </ul>
+          <a class="nav-link active me-2" aria-current="page" href="login.php">
+                <div>
+                  <button class="btn btn-secondary " type="button"style="background-color: white;color: #0200C3"; >
+                    <strong>Connexion</strong>
+                  </button>
+                </div>
+              </a>
+              <a class="nav-link active" aria-current="page" href="signup.php">
+                <div>
+                  <button class="btn btn-secondary " type="button"style="background-color: white;color: #0200C3"; >
+                    <strong>Inscription</strong>
+                  </button>
+                </div>
+              </a>
+          
+        </div>
+      </div>
+    </nav>
+  </header>
+  <div>
+  <img width="100%" height="520px" src="images/pexels-gabriel-peter-696644.jpg" alt="">
+</div>
+  
+  <div class="container text-center" style="margin-top:-300px ;color: white";>
+    <p>
+    <h1><strong >BIENVENUS SUR CE FORUM DE DISCUSSION</strong></h1>
+    </p>
+    <a class="nav-link active" aria-current="page" href="actions/logoutAction.php">
+                <div>
+                  <button class="btn btn-secondary " type="button"style="background-color: white;color: red"; >
+                    <strong>Deconnexion</strong>
+                  </button>
+                </div>
+              </a>
+  </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
