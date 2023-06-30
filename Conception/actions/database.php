270204a30d00875da589df552e72bd06473dcf0d
sabine <konkobosabine29@gmail.com>
2023-06-30 11:55:47 +0000

    modification

--- Conception/actions/database.php
+++ Conception/actions/database.php
@@ -1,6 +1,6 @@
 <?php   
 try{
-    session_start();
+    // session_start();
     $bdd  = new PDO('mysql:host=localhost;dbname=forum;charset=utf8;', 'root', '');
     }catch(Exception $e){
 die('une erreur a été retrouvé : ' . $e->getMessage()); 
