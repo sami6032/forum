4b08bf38d96cc8003f4eba07625f4a4c9277af7f
sabine <konkobosabine29@gmail.com>
2023-06-30 03:07:49 +0000

    modification

--- Conception/actions/questions/publishQuestionAction.php
+++ Conception/actions/questions/publishQuestionAction.php
@@ -29,38 +29,4 @@ if(isset($_POST['validate'])){
   }else{
          $errorMsg = "veuillez compléter tous les champs...";
 }
-
-// require('actions/database.php'); 
-
-// if(isset($_POST['validate'])){
-
-//     if(!empty($_POST['title']) AND !empty($_POST['description']) AND !empty($_POST['content']) AND !empty($_POST['id_auteur'])) {
-//       $question_title = htmlspecialchars($_POST['title']);
-//       $question_description = nl2br(htmlspecialchars($_POST['description']));
-//       $question_contenu = nl2br(htmlspecialchars($_POST['content']));
-//       $question_date = date('d/m/y');
-//       $question_id_auteur = $_SESSION['id_auteur'];
-//       $question_pseudo_auteur = $_SESSION['pseudo'];
-      
-//       $insertQuestionOnwebsite = $bdd->prepare('INSERT INTO questions(titre, description, contenu, id_auteur, pseudo_auteur, date_publication) VALUES(?, ?, ?, ?, ?, ?)');
-//       $insertQuestionOnwebsite->execute(
-//         array(
-//             $question_title,
-//             $question_description,
-//             $question_contenu,
-//             $question_id_auteur, 
-//             $question_pseudo_auteur,
-//             $question_date
-//         )
-//       );
-
-//       $successMsg = "Votre question a bien été publiée sur le site.";
-     
-//     } else {
-//       $errorMsg = "Veuillez compléter tous les champs...";
-//     }
-// }
-
-
-
 ?>
